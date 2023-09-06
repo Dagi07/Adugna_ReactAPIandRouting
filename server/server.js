737b063340954bee08243bff986c36e12f110d04
unknown <Administrator@BV.HOTEL>
2023-09-06 17:44:06 +0300

    some update on db insertion

--- server/server.js
+++ server/server.js
@@ -95,57 +95,59 @@ conn.query(ProductPrice, (err) => {
 //   console.log("Orders Table created");
 // });
 
-let prod1 = `INSERT INTO Products (product_url, product_name) VALUES ('iphonese', 'iPhone SE')`;
+app.get("/addIphones", (req, res) => {
+  let prod1 = `INSERT INTO Products (product_url, product_name) VALUES ('iphonese', 'iPhone SE')`;
 
-let prod2 = `INSERT INTO Products (product_url, product_name) VALUES ('iphone11', 'iPhone 11')`;
+  let prod2 = `INSERT INTO Products (product_url, product_name) VALUES ('iphone11', 'iPhone 11')`;
 
-let prod3 = `INSERT INTO Products (product_url, product_name) VALUES ('iphone11pro', 'iPhone 11 Pro')`;
+  let prod3 = `INSERT INTO Products (product_url, product_name) VALUES ('iphone11pro', 'iPhone 11 Pro')`;
 
-let desc1 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('1', 'Lots to love. Less to spend.', 'iPhone SE packs our most powerful chip into our most popular size at our most affordable price. It’s just what you’ve been waiting for.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_se__barrz3dlvxea_small_2x.jpg', 'https://www.apple.com/shop/buy-iphone/iphone-11')`;
+  let desc1 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('1', 'Lots to love. Less to spend.', 'iPhone SE packs our most powerful chip into our most popular size at our most affordable price. It’s just what you’ve been waiting for.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_se__barrz3dlvxea_small_2x.jpg', 'https://www.apple.com/shop/buy-iphone/iphone-11')`;
 
-let desc2 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('2', 'Lots to love. Less to spend.', 'You can either pay for your new iPhone in full or pay monthly with carrier financing, iPhone Payments, the iPhone Upgrade Program, and now Apple Card Monthly Installments. Your carrier service plan will be charged separately. Just choose the option that works for you.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_11__bsnxu2gpf98i_small_2x.jpg', 'https://www.apple.com/shop/buy-iphone/iphone-11')`;
+  let desc2 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('2', 'Lots to love. Less to spend.', 'You can either pay for your new iPhone in full or pay monthly with carrier financing, iPhone Payments, the iPhone Upgrade Program, and now Apple Card Monthly Installments. Your carrier service plan will be charged separately. Just choose the option that works for you.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_11__bsnxu2gpf98i_small_2x.jpg', 'https://www.apple.com/shop/buy-iphone/iphone-11')`;
 
-let desc3 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('3', 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro.', 'Pro cameras. Pro display. Pro performance.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_11_pro__d2lf9zrtcv8m_small_2x.jpg','https://www.apple.com/shop/buy-iphone/iphone-11-pro')`;
+  let desc3 = `INSERT INTO ProductDescription (product_id, product_brief_description, product_description, product_img, product_link) VALUES ('3', 'A transformative triple‑camera system that adds tons of capability without complexity. An unprecedented leap in battery life. And a mind‑blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro.', 'Pro cameras. Pro display. Pro performance.', 'https://www.apple.com/v/iphone/home/aj/images/overview/hero/hero_iphone_11_pro__d2lf9zrtcv8m_small_2x.jpg','https://www.apple.com/shop/buy-iphone/iphone-11-pro')`;
 
-let pric1 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('1', '$399', 'From $9.54/mo. or $229 with trade-in.*')`;
+  let pric1 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('1', '$399', 'From $9.54/mo. or $229 with trade-in.*')`;
 
-let pric2 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('2', '$449', 'From $18.70/mo. or $449 with trade-in.*')`;
+  let pric2 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('2', '$449', 'From $18.70/mo. or $449 with trade-in.*')`;
 
-let pric3 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('3', '$679', 'From $28.29/mo. or $679 with trade-in.*')`;
+  let pric3 = `INSERT INTO ProductPrice (product_id, starting_price, price_range) VALUES ('3', '$679', 'From $28.29/mo. or $679 with trade-in.*')`;
 
-conn.query(prod1, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-  console.log("product 1 inserted");
-});
+  conn.query(prod1, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+    console.log("product 1 inserted");
+  });
 
-conn.query(prod2, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
+  conn.query(prod2, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 
-conn.query(prod3, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
+  conn.query(prod3, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 
-conn.query(desc1, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
+  conn.query(desc1, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 
-conn.query(desc2, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
+  conn.query(desc2, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 
-conn.query(desc3, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
+  conn.query(desc3, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 
-conn.query(pric1, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
-conn.query(pric2, (err, results) => {
-  if (err) console.error(`error: ${err}`);
-});
-conn.query(pric3, (err, results) => {
-  if (err) console.error(`error: ${err}`);
+  conn.query(pric1, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
+  conn.query(pric2, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
+  conn.query(pric3, (err, results) => {
+    if (err) console.error(`error: ${err}`);
+  });
 });
 
 app.get("/iphone", (req, res) => {
